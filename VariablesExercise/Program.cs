3be46769c45d5180c22b5332b320f7b8bd2e1f63
juliannegodsey1 <juliannegodseyy@gmail.com>
2023-09-11 16:48:56 -0400

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿string carModel = "Volkswagon";
+int year = 2021;
+char trim = 'S';
+bool neverBeenInAccident = true;
+double miles = 12987.9;
+decimal monthlyCost = 399.99m;
+
+Console.WriteLine($"Car Model: {carModel}");
+Console.WriteLine($"Production Year: {year}");
+Console.WriteLine($"Trim: {trim}");
+Console.WriteLine($"Accident-Free? {neverBeenInAccident}");
+Console.WriteLine($"Mileage: {miles}");
+Console.WriteLine($"Estimated Monthly Cost: {monthlyCost}");
